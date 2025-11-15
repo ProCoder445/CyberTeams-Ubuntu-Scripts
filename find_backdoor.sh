b9ab0129c5c8a9694bd16a4c209a43c5f691d510
unknown <CZachariah28@RBRHS.local>
2025-11-15 08:40:24 -0500

    Adding backdoor script

---
+++ find_backdoor.sh
@@ -0,0 +1,4 @@
+echo "then run cd /etc && ls | grep | \"cron\" then \"ps aux | grep \" python\" After that, kill the process using the PID. The program will take you to the required page (not /etc/)"; 
+ 
+sudo su
+
