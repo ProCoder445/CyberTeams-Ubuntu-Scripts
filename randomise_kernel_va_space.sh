a2ee8de253e8a589caeb98024d1df6097d191895
ProCoder445 <cszachariah@gmail.com>
2025-12-13 18:23:56 +0000

    Add files via upload

---
+++ randomise_kernel_va_space.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sysctl -w kernel.randomize_va_space=2
+
+
+for file in /etc/sysctl.conf /etc/sysctl.d/*.conf /usr/lib/sysctl.d/*.conf /usr/local/lib/sysctl.d/*.conf /run/sysctl.d/*.conf; do
+	if [ -f "$file" ]; then
+		grep -Esq "^\s*kernel\.randomize_va_space\s*=\s*([0-1]|[3-9]|[1-9][0-9]+)" "$file" && sed -ri 's/^\s*kernel\.randomize_va_space\s*=\s*([0-1]|[3-9]|[1-9][0-9]+)/# &/gi' "$file"
+	fi
+done
+
